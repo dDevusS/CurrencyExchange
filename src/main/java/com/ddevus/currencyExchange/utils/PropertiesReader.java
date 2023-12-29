7e68b8f56ac4ec055151d01fdcef24026a7d5bdd
dDevusS <geckahilden@gmail.com>
2023-12-29 10:21:06 -0500

    Created PropertiesReader

--- src/main/java/com/ddevus/currencyExchange/utils/PropertiesReader.java
+++ src/main/java/com/ddevus/currencyExchange/utils/PropertiesReader.java
@@ -1,2 +1,29 @@
-package com.ddevus.currencyExchange.utils;public class PropertiesReader {
+package com.ddevus.currencyExchange.utils;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertiesReader {
+    private final static Properties PROPERTIES = new Properties();
+
+    public static String read(String key) {
+        return PROPERTIES.getProperty(key);
+    }
+
+    static {
+        loadPropertiesFile();
+    }
+
+    private static void loadPropertiesFile() {
+        try (var inputStream =
+                PropertiesReader.class.getClassLoader()
+                        .getResourceAsStream("application.properties")) {
+                PROPERTIES.load(inputStream);
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private PropertiesReader() {}
 }
