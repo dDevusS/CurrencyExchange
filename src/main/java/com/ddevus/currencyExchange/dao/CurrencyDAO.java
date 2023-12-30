5a6d9fec904cbc49b9cf0da84815eda8fd3604d0
dDevusS <geckahilden@gmail.com>
2023-12-29 20:04:56 -0500

    Created interface CurrencyDAO

---
+++ src/main/java/com/ddevus/currencyExchange/dao/CurrencyDAO.java
@@ -0,0 +1,14 @@
+package com.ddevus.currencyExchange.dao;
+
+import com.ddevus.currencyExchange.entity.CurrencyEntity;
+
+import java.util.List;
+
+public interface CurrencyDAO {
+
+    public CurrencyEntity save(CurrencyEntity currency);
+
+    public CurrencyEntity findById(int id);
+
+    public List<CurrencyEntity> findAll();
+}
