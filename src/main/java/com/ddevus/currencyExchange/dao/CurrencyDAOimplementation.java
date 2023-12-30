1e0b6266ed0144b62a0ca1f08cf52b3899b544ee
dDevusS <geckahilden@gmail.com>
2023-12-29 20:10:51 -0500

    Created CurrencyDAOimplementation without realization

---
+++ src/main/java/com/ddevus/currencyExchange/dao/CurrencyDAOimplementation.java
@@ -0,0 +1,28 @@
+package com.ddevus.currencyExchange.dao;
+
+import com.ddevus.currencyExchange.entity.CurrencyEntity;
+
+import java.util.List;
+
+public class CurrencyDAOimplementation implements CurrencyDAO {
+
+    @Override
+    public CurrencyEntity save(CurrencyEntity currency) {
+        return null;
+    }
+
+    @Override
+    public CurrencyEntity findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<CurrencyEntity> findAll() {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+}
