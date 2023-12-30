cf55f50aa2b80aa8137902dd85517b441377895e
dDevusS <geckahilden@gmail.com>
2023-12-29 19:41:46 -0500

    Created CurrencyEntity

---
+++ src/main/java/com/ddevus/currencyExchange/entity/CurrencyEntity.java
@@ -0,0 +1,48 @@
+package com.ddevus.currencyExchange.entity;
+
+public class CurrencyEntity {
+
+    private int id;
+    private String code;
+    private String name;
+    private String sing;
+
+    public CurrencyEntity(int id, String code, String name, String sing) {
+        this.id = id;
+        this.code = code;
+        this.name = name;
+        this.sing = sing;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSing() {
+        return sing;
+    }
+
+    public void setSing(String sing) {
+        this.sing = sing;
+    }
+}
