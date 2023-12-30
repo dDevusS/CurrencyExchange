e2c1c1abbd58a77f0ff655cb7f3936afcf2c42ea
dDevusS <geckahilden@gmail.com>
2023-12-30 14:17:27 -0500

    Created ExchangeRateDTO

---
+++ src/main/java/com/ddevus/currencyExchange/dto/ExchangeRateDTO.java
@@ -0,0 +1,34 @@
+package com.ddevus.currencyExchange.dto;
+
+import com.ddevus.currencyExchange.entity.CurrencyEntity;
+
+public class ExchangeRateDTO {
+
+    private int Id;
+    private CurrencyEntity baseCurrency;
+    private CurrencyEntity targetCurrency;
+    private float rate;
+
+    public ExchangeRateDTO(int id, CurrencyEntity baseCurrency, CurrencyEntity targetCurrency, float rate) {
+        Id = id;
+        this.baseCurrency = baseCurrency;
+        this.targetCurrency = targetCurrency;
+        this.rate = rate;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public CurrencyEntity getBaseCurrency() {
+        return baseCurrency;
+    }
+
+    public CurrencyEntity getTargetCurrency() {
+        return targetCurrency;
+    }
+
+    public float getRate() {
+        return rate;
+    }
+}
