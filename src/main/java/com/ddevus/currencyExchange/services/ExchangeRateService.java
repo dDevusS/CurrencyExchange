52a66a346f4f57e49acaf6a94c7b52b882ad426a
dDevusS <geckahilden@gmail.com>
2023-12-30 13:55:51 -0500

    Created interface ExchangeRateService

--- src/main/java/com/ddevus/currencyExchange/services/ExchangeRateService.java
+++ src/main/java/com/ddevus/currencyExchange/services/ExchangeRateService.java
@@ -1,2 +1,16 @@
-package com.ddevus.currencyExchange.services;public interface ExchangeRateService {
+package com.ddevus.currencyExchange.services;
+
+import com.ddevus.currencyExchange.entity.ExchangeRateEntity;
+
+import java.util.List;
+
+public interface ExchangeRateService {
+
+    public ExchangeRateEntity save(ExchangeRateEntity exchangeRate);
+
+    public ExchangeRateEntity findByBaseAndTargetCurrenciesId(int baseCurrencyId, int targetCurrencyId);
+
+    public List<ExchangeRateEntity> findAll();
+
+    public ExchangeRateEntity update(float rate);
 }
