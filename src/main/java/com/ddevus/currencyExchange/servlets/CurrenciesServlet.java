b64bc4c53203bcfc30f15c2c8dbb11bad5b5cdb7
dDevusS <geckahilden@gmail.com>
2023-12-30 19:42:54 -0500

    Created CurrenciesServlet

--- src/main/java/com/ddevus/currencyExchange/servlets/CurrenciesServlet.java
+++ src/main/java/com/ddevus/currencyExchange/servlets/CurrenciesServlet.java
@@ -1,17 +1,45 @@
 package com.ddevus.currencyExchange.servlets;
 
+import com.ddevus.currencyExchange.entity.CurrencyEntity;
+import com.ddevus.currencyExchange.services.CurrencyService;
+import com.ddevus.currencyExchange.services.CurrencyServiceImplementation;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @WebServlet("/currencies")
-public class CurrencyServlet extends HttpServlet {
+public class CurrenciesServlet extends HttpServlet {
+
+    private final CurrencyService currencyService = CurrencyServiceImplementation.getINSTANCE();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
 
+        List<CurrencyEntity> currencies = currencyService.findAll();
+        var json = ConvertListToJson(currencies);
+
+        resp.getWriter().write(json);
+    }
+
+    private String ConvertListToJson(List<CurrencyEntity> currencies) {
+        StringBuilder json = new StringBuilder("[");
+
+        for (CurrencyEntity currency : currencies) {
+            json.append(currency).append(",");
+        }
+
+        if (json.length() > 1) {
+            json.setCharAt(json.length() - 1, ']');
+        }
+
+        return json.toString();
     }
 }
