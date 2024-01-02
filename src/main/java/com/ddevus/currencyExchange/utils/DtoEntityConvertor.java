19732ba96aa1f3a653ac6c9cb65b69d45bc92ee2
dDevusS <geckahilden@gmail.com>
2024-01-01 19:09:53 -0500

    Add method for converting ExchangeRateDTO to ExchangeRateEntity

--- src/main/java/com/ddevus/currencyExchange/utils/DtoEntityConvertor.java
+++ src/main/java/com/ddevus/currencyExchange/utils/DtoEntityConvertor.java
@@ -1,2 +1,33 @@
-package com.ddevus.currencyExchange.utils;public class DtoEntityConvertor {
+package com.ddevus.currencyExchange.utils;
+
+import com.ddevus.currencyExchange.dto.CurrencyDTO;
+import com.ddevus.currencyExchange.dto.ExchangeRateDTO;
+import com.ddevus.currencyExchange.entity.CurrencyEntity;
+import com.ddevus.currencyExchange.entity.ExchangeRateEntity;
+
+public class DtoEntityConvertor {
+
+    public static CurrencyEntity convertCurrencyDtoToEntity(CurrencyDTO currencyDTO) {
+        return new CurrencyEntity(currencyDTO.getId()
+                , currencyDTO.getCode()
+                , currencyDTO.getName()
+                , currencyDTO.getSing());
+    }
+
+    public static CurrencyDTO convertCurrencyEntityToDto(CurrencyEntity currencyEntity) {
+        return new CurrencyDTO(currencyEntity.getId()
+                , currencyEntity.getName()
+                , currencyEntity.getCode()
+                , currencyEntity.getSing());
+    }
+
+    public static ExchangeRateEntity convertExchangeRateDtoToEntity(ExchangeRateDTO exchangeRateDTO) {
+        ExchangeRateEntity exchangeRateEntity
+                = new ExchangeRateEntity(exchangeRateDTO.getId()
+                , exchangeRateDTO.getBaseCurrency().getId()
+                , exchangeRateDTO.getTargetCurrency().getId()
+                , exchangeRateDTO.getRate());
+
+        return exchangeRateEntity;
+    }
 }
