3b96cf0a746141341eef924eb3625651858a74b3
dDevusS <geckahilden@gmail.com>
2024-01-05 15:20:11 -0500

    Fixed update() in ExchangeRate_Service

--- src/main/java/com/ddevus/currencyExchange/services/ExchangeRate_Service.java
+++ src/main/java/com/ddevus/currencyExchange/services/ExchangeRate_Service.java
@@ -2,29 +2,32 @@ package com.ddevus.currencyExchange.services;
 
 import com.ddevus.currencyExchange.dao.CurrencyDAO;
 import com.ddevus.currencyExchange.dao.ExchangeRateDAO;
+import com.ddevus.currencyExchange.dao.interfaces.ICurrencyDAO;
+import com.ddevus.currencyExchange.dao.interfaces.IExchangeRateDAO;
 import com.ddevus.currencyExchange.entity.Currency;
 import com.ddevus.currencyExchange.entity.ExchangeRate;
 import com.ddevus.currencyExchange.exceptions.SQLBadRequestException;
 import com.ddevus.currencyExchange.exceptions.WrapperException;
+import com.ddevus.currencyExchange.services.interfaces.IExchangeRate_Service;
 
 import java.math.BigDecimal;
 import java.util.List;
 
-public class ExchangeRate_Service implements com.ddevus.currencyExchange.services.interfaces.IExchangeRate_Service {
+public class ExchangeRate_Service implements IExchangeRate_Service {
 
-    private static final ExchangeRateDAO I_EXCHANGE_RATE_DAO = ExchangeRateDAO.getINSTANCE();
-    private static final CurrencyDAO I_CURRENCY_DAO = CurrencyDAO.getINSTANCE();
-    private static final com.ddevus.currencyExchange.services.interfaces.IExchangeRate_Service INSTANCE= new ExchangeRate_Service();
+    private static final IExchangeRateDAO exchangeRateDAO = ExchangeRateDAO.getINSTANCE();
+    private static final ICurrencyDAO currencyDAO = CurrencyDAO.getINSTANCE();
+    private static final IExchangeRate_Service INSTANCE= new ExchangeRate_Service();
 
     private ExchangeRate_Service() {}
 
-    public static com.ddevus.currencyExchange.services.interfaces.IExchangeRate_Service getINSTANCE() {
+    public static IExchangeRate_Service getINSTANCE() {
         return INSTANCE;
     }
 
     @Override
     public ExchangeRate save(ExchangeRate exchangeRate) {
-        var savedExchangeRateEntity = I_EXCHANGE_RATE_DAO.save(exchangeRate);
+        var savedExchangeRateEntity = exchangeRateDAO.save(exchangeRate);
         exchangeRate.setId(savedExchangeRateEntity.getId());
 
         return exchangeRate;
@@ -36,22 +39,21 @@ public class ExchangeRate_Service implements com.ddevus.currencyExchange.service
         Currency targetCurrency;
 
         try {
-            baseCurrency = I_CURRENCY_DAO.findByCode(baseCurrencyCode);
-            targetCurrency = I_CURRENCY_DAO.findByCode(targetCurrencyCode);
+            baseCurrency = currencyDAO.findByCode(baseCurrencyCode);
+            targetCurrency = currencyDAO.findByCode(targetCurrencyCode);
         }
         catch (SQLBadRequestException e) {
             throw new SQLBadRequestException("There is no currency or currencies with those codes."
             , WrapperException.ErrorReason.FAILED_FIND_CURRENCY_IN_DB);
         }
 
-        return I_EXCHANGE_RATE_DAO.findByBaseAndTargetCurrencies(baseCurrency, targetCurrency);
+        return exchangeRateDAO.findByBaseAndTargetCurrencies(baseCurrency, targetCurrency);
     }
 
     @Override
     public List<ExchangeRate> findAll() {
-        List<ExchangeRate> exchangeRateList = I_EXCHANGE_RATE_DAO.findAll();
 
-        return exchangeRateList;
+        return exchangeRateDAO.findAll();
     }
 
     @Override
@@ -59,13 +61,14 @@ public class ExchangeRate_Service implements com.ddevus.currencyExchange.service
         ExchangeRate exchangeRate;
 
         try {
-            Currency baseCurrency = I_CURRENCY_DAO.findByCode(baseCurrencyCode);
-            Currency targetCurrency = I_CURRENCY_DAO.findByCode(targetCurrencyCode);
-            exchangeRate = I_EXCHANGE_RATE_DAO
+            Currency baseCurrency = currencyDAO.findByCode(baseCurrencyCode);
+            Currency targetCurrency = currencyDAO.findByCode(targetCurrencyCode);
+            exchangeRate = exchangeRateDAO
                     .findByBaseAndTargetCurrencies(baseCurrency,
                             targetCurrency);
 
-            I_EXCHANGE_RATE_DAO.update(exchangeRate.getId(), rate);
+            exchangeRateDAO.update(exchangeRate.getId(), rate);
+            exchangeRate.setRate(rate);
         }
         catch (SQLBadRequestException e) {
             throw new SQLBadRequestException("There is no currency pair with those codes in the database."
